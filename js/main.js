1cd40395003a0bdca33e6a1ebe3cab42bbb43541
Dron <refaced@nextmail.ru>
2019-10-28 21:40:33 +0300

    Это первый комит

---
+++ js/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+
+/*var number = 0;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+let person = {
+   name: "john",
+   age: "25",
+   isMarried: false
+};
+
+console.log(person["name"]);
+
+
+
+let arr = ['plum.jpg','orange.png','apple.bmp'];
+console.log(arr[2]);*/
+
+// alert("Hello World"); 
+
+//let answer = confirm("Are you here?"); 
+
+//console.log(answer);
+
+/*let answer = +prompt("Are you here?", "YEs");
+console.log(typeof(answer));
+
+console.log("arr" + "object");*/
+
+let incr = 10,
+    decr = 10;
+
+ 
+
+    console.log(++incr);
+    console.log(--decr);
